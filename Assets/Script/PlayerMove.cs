3e1b98c5ef468c54dc1101e9083ebe1578c0f178
fgl0001 <hiro.58.fgl.2525.952@icloud.com>
2022-01-17 19:15:08 +0900

    コード変更中

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -8,9 +8,11 @@ public class PlayerMove : MonoBehaviour
     [SerializeField]
     float speed=1f;
     Vector3 pos;
+    public Rigidbody rb;
     void Start()
     {
         pos = transform.position;
+        rb=GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,18 +22,25 @@ public class PlayerMove : MonoBehaviour
         var forece = new Vector3(rl, 0f, 0f);
         GetComponent<Rigidbody>().AddForce(forece * 2000);*/
 
-        if(Input.GetKey("a"))
+        /*if(Input.GetKey("a"))
         {
             transform.rotation = Quaternion.Euler(0, 90, 0);//‰ñ“]
-            transform.position+=transform.forward*speed;
+            transform.position+=transform.forward*speed*Time.deltaTime;
         }
         if (Input.GetKey("d"))
         {
             transform.rotation = Quaternion.Euler(0, -90, 0);//‰ñ“]
             transform.position += transform.forward * speed;
-        }
+        }*/
+        
 
 
     }
-    
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizotal");
+        transform.rotation = Quaternion.Euler(0, 90, 0);
+        rb.AddForce(x, 0, 0);
+    }
+
 }
